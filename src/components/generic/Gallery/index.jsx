e525b1813dcc1f0ffecdac005948082ff18d53a9
Valentin FEKRACH <valentin.fekrach@shadline.com>
2022-12-26 17:04:10 +0100

    feat: gallery generic component

---
+++ src/components/generic/Gallery/index.jsx
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+const Gallery = ({ alt, className, pictures }) => {
+  const [index, setIndex] = useState(0);
+  const maxIndex = useMemo(() => pictures.length - 1, [pictures]);
+
+  const handlePictureChange = useCallback(
+    (number) => {
+      const definedNumber = Math.sign(number);
+      if (isNaN(definedNumber) || definedNumber === 0)
+        throw new Error('Given number is NaN');
+      const isIndexZero = index === 0;
+      const isLastIndex = index === maxIndex;
+      if (definedNumber === 1) {
+        isLastIndex ? setIndex(0) : setIndex(index + 1);
+      }
+      if (definedNumber === -1) {
+        isIndexZero ? setIndex(maxIndex) : setIndex(index - 1);
+      }
+    },
+    [index, maxIndex]
+  );
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      handlePictureChange(1);
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, [index, handlePictureChange]);
+
+  return (
+    <div className={`gallery relative ${className || ''}`}>
+      <button
+        aria-controls='gallery-img'
+        className='gallery-controls prev'
+        onClick={() => handlePictureChange(-1)}
+      ></button>
+      <button
+        aria-controls='gallery-img'
+        className='gallery-controls next'
+        onClick={() => handlePictureChange(1)}
+      ></button>
+      <img id='gallery-img' src={pictures[index]} alt={alt} />
+    </div>
+  );
+};
+
+Gallery.propTypes = {
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  pictures: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
+
+export default Gallery;
